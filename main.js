659af3ed035e03d8a97c32745e831603fa029adc
Ahn-GiHwan <61266464+Ahn-GiHwan@users.noreply.github.com>
2021-03-29 16:13:50 +0900

    regexp

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log('hello')
+const str = `
+  010-1234-5678
+  thesecon@gmail.com
+  https://www.omdbapi.com/?apikey=7035c60c&s=frozon
+  The quick brown fox jumps over the lazy dog.
+  abbcccdddd
+`
+
+console.log(
+  str.match(/(?<=@).{1,}/g)
+)
